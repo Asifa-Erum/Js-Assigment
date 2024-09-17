07c71c3fe259f0eadac0037fd8ff72050746f6a6
Asifa-Erum <asifaerum2506@gmail.com>
2024-09-17 17:28:20 +0500

    Add files via upload

---
+++ js assigment -asifa -(2)/index.js
@@ -0,0 +1,37 @@
+//   QNO:1   //
+var name = "Asifa";
+alert(name);
+// ----------------//
+
+//    QNO:2   //
+
+var name = "Asifa";
+var fullName="Erum";
+console.log(name + " " + fullName);
+// ----------------//
+
+
+//    QNO:3    //
+ 
+var a =4;
+var b =2;
+ console.log
+ (a++ + --b - --a + b++ + a+b);
+
+//  QNO:  4  //
+var num1=4;
+var num2=5;
+var sum=num1+num2;
+var user=prompt("Solve the statement" + " " + 4 + " " +"+" + 5)
+if (user == 9) {
+    console.log("Yes your ans is right!");
+}else if (user != 9){
+    console.log("Your ans is wrong!");
+
+}else{
+    console.log("Try again!")
+}
+
+
+
+
